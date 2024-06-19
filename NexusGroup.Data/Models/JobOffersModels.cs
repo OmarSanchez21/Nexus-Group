efd6fe0a5f44bf595f265a1ee0165e07e2d94618
OmarSanchez21 <omisp21@gmail.com>
2024-06-18 22:55:47 -0400

    empezando modelo de jobOffers

---
+++ NexusGroup.Data/Models/JobOffersModels.cs
@@ -0,0 +1,20 @@
+﻿using NexusGroup.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusGroup.Data.Models
+{
+    public class JobOffersModels: BaseData
+    {
+        [Key]
+        public int offerID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateOnly publicationDate { get; set; }
+        public DateOnly clouserDate { get; set; }
+    }
+}
