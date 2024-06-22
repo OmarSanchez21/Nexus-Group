2ed56aa454d5073b3b60629e5c5cf66aa8c3245c
OmarSanchez21 <omisp21@gmail.com>
2024-06-22 19:45:34 -0400

    Empezando elempleado

---
+++ NexusGroup.Data/Models/EmployeesModels.cs
@@ -0,0 +1,24 @@
+﻿using NexusGroup.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusGroup.Data.Models
+{
+    public class EmployeesModels: BaseData
+    {
+        [Key]
+        public int employeeID { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string photo { get; set; }
+        public string email { get; set; }
+        public string passwordHash { get; set; }
+        public DateTime joinDate { get; set; }
+        public int positionID { get; set; }
+        public int accessLevelID { get; set; }
+    }
+}
---
+++ NexusGroup.Data/Repositories/Employees/IEmployeesRepositories.cs
@@ -0,0 +1,14 @@
+﻿using NexusGroup.Data.Base;
+using NexusGroup.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusGroup.Data.Repositories.Employees
+{
+    public interface IEmployeesRepositories: IService<EmployeesModels>
+    {
+    }
+}
